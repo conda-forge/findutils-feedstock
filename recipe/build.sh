a9126e360f4dc37a39f98bd4e284231adbb16f06
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-30 16:33:37 -0800

    update to 4.10.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,11 @@ sed -i '/tests\/xargs\/test-sigusr/d' tests/local.mk
 sed -i '/tests\/find\/used.sh/d' tests/local.mk
 sed -i '/tests\/find\/newer.sh/d' tests/local.mk
 
+if [[ ${target_platform} == "linux-aarch64" || ${target_platform} == "linux-ppc64le" ]]; then
+    sed -i '/tests\/find\/depth-unreadable-dir/d' tests/local.mk
+    sed -i '/tests\/xargs\/conflicting_opts/d' tests/local.mk
+fi
+
 autoreconf --force --verbose --install
 ./configure --prefix="$PREFIX" --disable-dependency-tracking
 make -j${CPU_COUNT}
